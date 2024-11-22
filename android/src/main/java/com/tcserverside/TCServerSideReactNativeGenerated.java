84b199f01e5b93d983082fdb4aa6cd7f685d16dc
ahakimsaid <abdelhakim.said@commandersact.com>
2024-11-22 16:10:24 +0100

    1.2.5 (#9)

--- android/src/main/java/com/tcserverside/TCServerSideReactNativeGenerated.java
+++ android/src/main/java/com/tcserverside/TCServerSideReactNativeGenerated.java
@@ -6,5 +6,5 @@ package com.tcserverside;
 
 public class TCServerSideReactNativeGenerated
 {
-    public static final String version = "1.2.4";
+    public static final String version = "1.2.5";
 }
--- ios/TCServerSideGenerated.swift
+++ ios/TCServerSideGenerated.swift
@@ -6,5 +6,5 @@ import Foundation
 
 struct TCServerSideReactNativeGenerated
 {
-    static let version = "1.2.4"
+    static let version = "1.2.5"
 }
